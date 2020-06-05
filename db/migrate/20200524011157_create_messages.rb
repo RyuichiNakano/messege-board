66e0f6bbbc2e3d94d200b832bc0548647e9b874a
ryuichinakano <blowinblues582@gmail.com>
2020-06-05 08:29:31 +0000

    seedの活用とページネーションの作成

--- db/migrate/20200524011157_create_messages.rb
+++ db/migrate/20200524011157_create_messages.rb
@@ -2,9 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.string :content
-
+      # t.string :title
       t.timestamps
     end
   end
 end
-#ActiveRecord
